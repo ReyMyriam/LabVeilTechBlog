c09fc77af2ac99aba851de11daa020faa4ae7c20
ReyMyriam <myriam.rey@heig-vd.ch>
2024-01-21 18:06:39 +0100

    Update rss.xml.js with repo name base

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `LabVeilTech_AstroBlog/blog/${post.slug}/`,
+			link: `LabVeilTechBlog/blog/${post.slug}/`,
 		})),
 	});
 }
