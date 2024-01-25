1ecbeb1119affceb54b5a962c2f0a04f0bef4c78
ReyMyriam <myriam.rey@heig-vd.ch>
2024-01-25 09:15:29 +0100

    Update astro.config.mjs test url and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://reymyriam.github.io',
-  	base: '/LabVeilTechBlog',
+	site: 'https://reymyriam.github.io/LabVeilTechBlog',
+  	base: '/',
 });
